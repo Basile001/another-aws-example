57356444e26e2bd147bc9966a470cb4094fc0e92
basile grandperret <basile.grandperret@hotmail.fr>
2022-11-08 13:55:02 +0100

    config prod

--- cdk/env/prod/aws.ts
+++ cdk/env/prod/aws.ts
@@ -1,7 +1,7 @@
 export const COGNITO = {
   REGION: "eu-west-3",
-  USER_POOL_ID: "",
-  APP_CLIENT_ID: "",
+  USER_POOL_ID: "eu-west-3_9efwFiijf",
+  APP_CLIENT_ID: "33jecsaosnimf7ln8flrmgc6kd",
 };
 
-export const API_URL = "";
+export const API_URL = "https://pdh2elkta9.execute-api.eu-west-3.amazonaws.com/prod";
