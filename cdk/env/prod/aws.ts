7397ff05adcf0d187c09ee4942bc170082c919c5
basile grandperret <basile.grandperret@hotmail.fr>
2022-11-14 15:03:05 +0100

    revert change

--- cdk/env/prod/aws.ts
+++ cdk/env/prod/aws.ts
@@ -1,7 +1,7 @@
 export const COGNITO = {
   REGION: "eu-west-3",
-  USER_POOL_ID: "eu-west-3_9efwFiijf",
-  APP_CLIENT_ID: "33jecsaosnimf7ln8flrmgc6kd",
+  USER_POOL_ID: "eu-west-3_JGwC2YNXV",
+  APP_CLIENT_ID: "72srd9onu8r4idh66pahd5lg97",
 };
 
-export const API_URL = "https://pdh2elkta9.execute-api.eu-west-3.amazonaws.com/prod";
+export const API_URL = "https://u7dst3p3bk.execute-api.eu-west-3.amazonaws.com/prod";
